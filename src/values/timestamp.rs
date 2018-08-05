de3edca72d735b00bf82db4cd03dbe6bc2ae3637
Mara Bos <m-ou.se@m-ou.se>
2018-08-05 14:35:08 +0200

    Fix Timestamp serialization and add tests.
    It panicked with a timestamp of 0.

--- src/values/timestamp.rs
+++ src/values/timestamp.rs
@@ -35,9 +35,11 @@ impl<'d> Argdata<'d> for Timestamp{
 		writer.write_all(&[9])?;
 		let nsec = self.nanoseconds();
 		let n = i128_serialized_length(nsec);
-		let mut buf = [0u8; 12];
-		BigEndian::write_int128(&mut buf, nsec, n);
-		writer.write_all(&buf)?;
+		if n > 0 {
+			let mut buf = [0u8; 12];
+			BigEndian::write_int128(&mut buf, nsec, n);
+			writer.write_all(&buf[..n])?;
+		}
 		Ok(())
 	}
 }
@@ -55,5 +57,21 @@ fn i128_serialized_length(v: i128) -> usize {
 		((128 - v.leading_zeros()) / 8 + 1) as usize
 	} else {
 		((128 - (!v).leading_zeros()) / 8 + 1) as usize
+	}
+}
+
+#[test]
+fn timestamp_serialize_test() {
+	for &(timespec, serialized) in &[
+		(Timespec{ sec:  0, nsec:           0 }, &b"\x09"[..]),
+		(Timespec{ sec:  0, nsec:           1 }, &b"\x09\x01"[..]),
+		(Timespec{ sec: -1, nsec: 999_999_999 }, &b"\x09\xFF"[..]),
+		(Timespec{ sec: 10, nsec:           0 }, &b"\x09\x02\x54\x0B\xE4\x00"[..]),
+	] {
+		let value = timestamp(timespec);
+		let mut buf = Vec::new();
+		value.serialize(&mut buf, None).unwrap();
+		assert_eq!(&buf, &serialized);
+		assert_eq!(value.serialized_length(), buf.len());
 	}
 }
